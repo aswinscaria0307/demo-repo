bc82ccfbf99952ee5bf7e46f1a82321e829c0916
aswin-michaels <aswin@michaels.com>
2025-06-05 21:41:52 +0530

    mik-grs-data-ingestion-api repo type changed

--- sql/changeset1.sql
+++ sql/changeset1.sql
@@ -1,6 +1,1 @@
--- liquibase formatted sql
--- changeset testuser:1 context=dev
-CREATE TABLE test_table (
-    id STRING(36) NOT NULL,
-    name STRING(255)
-) PRIMARY KEY (id);
+CREATE TABLE example_table (id STRING(36) NOT NULL) PRIMARY KEY (id);
